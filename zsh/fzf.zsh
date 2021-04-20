cb46e91bc2d38ae05ed5c1b5380d7495a03b399e
gin <ginxq@qq.com>
2021-04-20 14:00:37 +0800

    del zsh vi-mode

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,12 +1,17 @@
 # fzf
-
 export FZF_DEFAULT_COMMAND='
-fd --type f --hidden --follow --exclude={.git,node_modules,build} . /home
+fd --type f --hidden --follow --exclude={.git,node_modules} .
 '
+
+export FZF_COMPLETION_TRIGGER='\'
 
 export FZF_DEFAULT_OPTS='
 --height 50% 
 --layout=reverse 
---preview "[[ $(file {}) =~ binary ]] && echo {} is a binary file || (bat --theme=gruvbox --color=always {} || highlight -0 ansi -l {} || cat {}) 2> /dev/null | head -500"
 '
 
+# export FZF_DEFAULT_OPTS='
+# --height 50% 
+# --layout=reverse 
+# --preview "[[ $(file {}) =~ binary ]] && echo {} is a binary file || (bat --theme=gruvbox --color=always {} || highlight -0 ansi -l {} || cat {}) 2> /dev/null | head -500"
+# '
