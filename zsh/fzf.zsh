a3a31c669d1375d24ecc5fcb692c7dbe96babca5
gin <gin-18@qq.com>
2024-03-04 23:03:42 +0800

    change catppuccin palette to mocha

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -6,8 +6,8 @@ ag -l -U --hidden --ignore-dir node_modules --ignore-dir .git . /home
 export FZF_COMPLETION_TRIGGER='\'
 
 export FZF_DEFAULT_OPTS=" \
---color=bg+:#414559,bg:#303446,spinner:#f2d5cf,hl:#e78284 \
---color=fg:#c6d0f5,header:#e78284,info:#ca9ee6,pointer:#f2d5cf \
---color=marker:#f2d5cf,fg+:#c6d0f5,prompt:#ca9ee6,hl+:#e78284
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
 --preview 'bat --style=numbers --color=always --line-range :500 {}'
 "
