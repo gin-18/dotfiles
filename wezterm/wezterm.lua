f93517222248f5e734639cfed04527692a9f132b
gin <gin-18@qq.com>
2024-03-06 16:56:43 +0800

    update dunst and lazygit, set tab style for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,9 +10,9 @@ config.keys = keys
 -- 设置主题
 config.color_scheme = 'catppuccin-mocha'
 -- 设置字体
-config.font = wezterm.font('FiraCode Nerd Font', {
-  weight = 'Medium',
-})
+-- config.font = wezterm.font('FiraCode Nerd Font', {
+--   weight = 'Medium',
+-- })
 
 -- 设置默认启动程序
 config.default_prog = launch.default_prog
@@ -24,6 +24,34 @@ config.window_padding = {
   top = 0,
   right = 0,
   bottom = 0,
+}
+
+-- 设置tab
+config.use_fancy_tab_bar = false
+config.colors = {
+  tab_bar = {
+    background = '#45475a',
+    active_tab = {
+      bg_color = '#cba6f7',
+      fg_color = '#1e1e2e',
+    },
+    inactive_tab = {
+      bg_color = '#181825',
+      fg_color = '#a6adc8',
+    },
+    inactive_tab_hover = {
+      bg_color = '#313244',
+      fg_color = '#a6adc8',
+    },
+    new_tab = {
+      bg_color = '#181825',
+      fg_color = '#a6adc8',
+    },
+    new_tab_hover = {
+      bg_color = '#313244',
+      fg_color = '#a6adc8',
+    },
+  },
 }
 
 return config
