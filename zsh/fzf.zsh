b764c8d4f8bc65a762a27e4b107c5f445eabcb2f
gin <gin-18@qq.com>
2024-11-02 21:02:23 +0800

    feat: find directory with fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,13 +1,12 @@
-# fzf
-export FZF_DEFAULT_COMMAND='
-ag -l -U --hidden --ignore-dir node_modules --ignore-dir .git . /home
-'
-
-export FZF_COMPLETION_TRIGGER='\'
-
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
---color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
---preview 'bat --style=numbers --color=always --line-range :500 {}'
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi \
+"
+
+# select directory at $HOME
+export FZF_DEFAULT_COMMAND="
+find $HOME \( -name node_modules -o -name .git \) -prune -o -type d
 "
