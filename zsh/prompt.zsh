a73f4d577634e81889c2288db18cc824ca348c43
gin <gin-18@qq.com>
2024-11-03 14:02:43 +0800

    refactor: prompt

---
+++ zsh/prompt.zsh
@@ -0,0 +1,1 @@
+export PROMPT="%F{147}%n%f@%F{183}%m%f %F{225}%~%f \$ "
--- zsh/prompt_color.zsh
+++
@@ -1,9 +0,0 @@
-if [ -n "$BASH_VERSION" ]; then
-    export PS1='\[\e[38;5;147m\]\u\[\e[0m\]@\[\e[38;5;183m\]\h\[\e[0m\] \[\e[38;5;225m\]\w\[\e[0m\] \$ '
-else
-    if [ "$UID" -eq 0 ]; then
-        export PROMPT="%F{147}%n%f@%F{183}%m%f %F{225}%~%f %# "
-    else
-        export PROMPT="%F{147}%n%f@%F{183}%m%f %F{225}%~%f \$ "
-    fi
-fi
