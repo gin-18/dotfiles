ba9a95d41ef11d19a1b36003071f98086e06284e
gin <gin-18@qq.com>
2024-11-03 21:36:54 +0800

    move prompt in zshrc

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,10 +1,3 @@
-# plugin
-
-# zsh-autosuggestions
 zinit light zsh-users/zsh-autosuggestions
-
-# fast-syntax-highlighting
 zinit light zdharma-continuum/fast-syntax-highlighting
-
-# history-search-multi-word
 zinit load zdharma-continuum/history-search-multi-word
--- zsh/prompt.zsh
+++
@@ -1,1 +0,0 @@
-export PROMPT="%F{147}%n%f@%F{183}%m%f %F{225}%~%f \$ "
