fd67a9a91b8798f739741a6171bed389a2e8002f
gin <gin-18@qq.com>
2024-11-05 23:51:40 +0800

    remove zsh-syntax-highlighting

--- zsh/zsh-syntax-highlighting.zsh
+++
@@ -1,9 +0,0 @@
-if [ "$CATPPUCCIN_FLAVOR" = "frappe" ]; then
-    source ${0:A:h}/catppuccin-zsh-syntax-highlighting/catppuccin_frappe-zsh-syntax-highlighting.zsh
-elif [ "$CATPPUCCIN_FLAVOR" = "latte" ]; then
-    source ${0:A:h}/catppuccin-zsh-syntax-highlighting/catppuccin_latte-zsh-syntax-highlighting.zsh
-elif [ "$CATPPUCCIN_FLAVOR" = "macchiato" ]; then
-    source ${0:A:h}/catppuccin-zsh-syntax-highlighting/catppuccin_macchiato-zsh-syntax-highlighting.zsh
-else
-    source ${0:A:h}/catppuccin-zsh-syntax-highlighting/catppuccin_mocha-zsh-syntax-highlighting.zsh
-fi
